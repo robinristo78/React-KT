1fb7fd8e7e9c726938e55552146779afe22804bf
robinristo78 <robinristo@gmail.com>
2025-04-01 11:20:06 +0300

    Ülesanne 8 - useReducer hooki lisamine

--- src/App.js
+++ src/App.js
@@ -1,14 +1,55 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import Header from './components/Header';
 import Meals from './components/Meals';
-import { CartProvider } from './store/CartContext';
+import CartContext from './store/CartContext';
+
+const cartReducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD_ITEM': {
+      //kontrolli kas on olemas. Leia indexi kaudu, et saaks pärast uuendada selle kaudu.
+      const existItemIndex = state.items.findIndex(item => item.id === action.item.id);
+      const existItem = state.items[existItemIndex];
+      //
+      
+      let updatedItems;
+      
+      //Seega kui klikitud element juba eksisteerib kaardi elementide hulgas, 
+      // siis seda uuesti lisada pole vaja, 
+      // vaid suurendada selle kogus (quantity) ühe võrra.
+      if (existItem) {
+        const updatedItem = { ...existItem, quantity: existItem.quantity + 1 };
+        updatedItems = [...state.items];
+        updatedItems[existItemIndex] = updatedItem;
+      }
+      //Kui aga klikitud element veel ei eksisteeri, 
+      // siis lihtsalt lisame selle elementi kaardi elementide hulka, 
+      // määrates selle kogus 1-ga võrdseks.
+      else {
+        updatedItems = [...state.items, { ...action.item, quantity: 1 }];
+      }
+
+      return { items: updatedItems };
+    }
+
+    default:
+        return state;
+  }
+};
 
 const App = () => {
+  const [cartState, dispatchCart] = useReducer(cartReducer, {
+    items: []
+  });
+
+  const addItemHandler = (item) => {
+    dispatchCart({ type: 'ADD_ITEM', item });
+  };
+
  return (
-    <CartProvider>
+    <CartContext.Provider value={{ items: cartState.items, addItem: addItemHandler }}>
       <Header />
       <Meals />
-    </CartProvider>
+    </CartContext.Provider>
   );
 }
 
