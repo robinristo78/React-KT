244e7279cb4281815e7cdb2fe2be2177d0fc1366
robinristo78 <robinristo@gmail.com>
2025-04-01 09:17:40 +0300

    Ülesanne 3 - andmete pärimine Meals.js komponendis

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
 import Meals from './components/Meals';
-import MealItem from './components/MealItem';
 
 const App = () => {
  return (
--- src/components/Meals.js
+++ src/components/Meals.js
@@ -1,9 +1,24 @@
+import { useEffect } from "react";
+
 const Meals = () => {
     return (
         <ul id="meals">
-            <h2>create list of meals, using fetch data from backend.</h2>
+            {/* <h2>create list of meals, using fetch data from backend.</h2> */}
             { 
                 // list of meals
+                useEffect(() => {
+                    const fetchMeals = async () => {
+                        try {
+                            const response = await fetch("http://localhost:3001/meals");
+                            const data = await response.json();
+                            console.log("data", data);
+                        } catch (error) {
+                            console.error("Error fetching meals:", error);
+                        }
+                    }
+
+                    fetchMeals();
+                })
             }
         </ul>
     )
